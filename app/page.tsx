572c38e89b39f9a78feaa1fa557ed670693b6a64
David Makinde <58576209+daevid-thegreat@users.noreply.github.com>
2024-04-17 00:08:52 +0100

    population and about...

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import Hero from "@/components/Hero";
 import About from "@/components/About";
+import Population from "@/components/Population";
 
 export default function Home() {
   return (
-    <main className="font-ojuju">
+    <main className="font-ojuju overflow-y-auto">
         <Hero/>
         <About/>
+        <Population/>
     </main>
   );
 }
--- components/About.tsx
+++ components/About.tsx
@@ -1,8 +1,15 @@
+import Image from "next/image";
+
 export default function About() {
   return (
-      <div className={''}>
-         lorem ipsum dolor sit amet lorem lorem ipsum dolor sit amet lorem lorem ipsum dolor sit amet lorem lorem ipsum dolor sit amet lorem, lorem ipsum dolor sit amet lorem lorem ipsum dolor sit amet lorem lorem ipsum dolor sit amet lorem
+      <div className={'grid grid-cols-2 my-24 space-x-4 items-center px-16'}>
+        <div>
+             lorem ipsum dolor sit amet lorem lorem ipsum dolor sit amet lorem lorem ipsum dolor sit amet lorem lorem ipsum dolor sit amet lorem, lorem ipsum dolor sit amet lorem lorem ipsum dolor sit amet lorem lorem ipsum dolor sit amet lorem
           lorem ipsum dolor sit amet lorem lorem ipsum dolor sit amet lorem lorem ipsum dolor sit amet lorem lorem ipsum dolor sit amet lorem
+        </div>
+          <div className={'flex justify-center'}>
+              <Image src={'/group.png'} alt={'group'} width={450} height={450}/>
+              </div>
       </div>
   );
 }
