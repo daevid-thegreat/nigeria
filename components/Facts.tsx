336877aaa457e816dea8d147f0384b1df68b7f01
David Makinde <58576209+daevid-thegreat@users.noreply.github.com>
2024-04-17 11:35:53 +0100

    population and about and facts...

---
+++ components/Facts.tsx
@@ -0,0 +1,9 @@
+export default function Facts() {
+  return (
+      <div className={'px-16 py-14'}>
+     <p className={'text-4xl text-primary font-medium text-center'}>
+         Facts about Nigeria
+     </p>
+      </div>
+  );
+}
--- components/Population.tsx
+++ components/Population.tsx
@@ -1,13 +1,9 @@
-import Image from "next/image";
-
 export default function Population() {
   return (
       <div className={'items-center px-16 py-14 bg-primary text-center text-white'}>
-        <h1 className={'text-4xl font-semibold'}>230,000 Million People</h1>
+        <h1 className={'text-4xl font-semibold'}>200,000,000+ People</h1>
           <p className={'text-lg font-medium my-5'}>
                 Nigeria is the most populous country in Africa and the seventh most populous country in the world. She has a population of over 230 million people.
-              <br/>
-              The country is home to over ........
           </p>
       </div>
   );
