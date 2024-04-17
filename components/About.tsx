5f137b2581c39417b0cfa91ce9a76d90f81175cc
David Makinde <58576209+daevid-thegreat@users.noreply.github.com>
2024-04-17 15:19:03 +0100

    ermmmm...

--- components/About.tsx
+++ components/About.tsx
@@ -3,9 +3,14 @@ import Image from "next/image";
 export default function About() {
   return (
       <div className={'grid grid-cols-2 my-24 space-x-4 items-center px-16'}>
-        <div>
-             lorem ipsum dolor sit amet lorem lorem ipsum dolor sit amet lorem lorem ipsum dolor sit amet lorem lorem ipsum dolor sit amet lorem, lorem ipsum dolor sit amet lorem lorem ipsum dolor sit amet lorem lorem ipsum dolor sit amet lorem
-          lorem ipsum dolor sit amet lorem lorem ipsum dolor sit amet lorem lorem ipsum dolor sit amet lorem lorem ipsum dolor sit amet lorem
+        <div className={'text-xl text-justify'}>
+            On January 1, 1914, Lord Frederick Lugard, the governor of both the Northern Nigeria Protectorate and the Colony and Protectorate of Southern Nigeria, signed a document consolidating the two, thereby creating the Colony and Protectorate of Nigeria.
+            <br/>
+            The name Nigeria was coined in the late 19th century by British journalist Flora Shaw, She was the wife of Lord Lugard. The name comes from the Niger River, which runs through the country.
+            <br/>
+            Nigeria gained a degree of self-rule in 1954, and full independence from the United Kingdom on 1 October 1960, as the Federation of Nigeria with Abubakar Tafawa Balewa as its prime minister, while retaining the British monarch, Elizabeth II, as nominal head of state and Queen of Nigeria.
+
+
         </div>
           <div className={'flex justify-center'}>
               <Image src={'/group.png'} alt={'group'} width={450} height={450}/>
--- components/Facts.tsx
+++ components/Facts.tsx
@@ -1,9 +1,18 @@
 export default function Facts() {
   return (
-      <div className={'px-16 py-14'}>
+      <div className={'px-24 py-14'}>
      <p className={'text-4xl text-primary font-medium text-center'}>
-         Facts about Nigeria
+         Quick Facts
      </p>
+
+          <div className={'mt-8 text-primary text-xl font-medium space-y-4'}>
+              <p className={'text-start'}>*There are over 250 languages spoken in
+                  Nigeria and 271 ethnic groups*</p>
+              <p className={'text-center'}>*Nigeria is home to Aliko Dangote, the richest man in Africa*</p>
+              <p className={'text-end'}>*Nigeria has the highest rate of twin births per year globally*</p>
+
+
+          </div>
       </div>
   );
 }
